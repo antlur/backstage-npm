61475a586fe494404c2ad95304f42234fbc5aa43
Anthony Holmes <anthony@antlur.co>
2025-04-21 22:08:09 -0500

    Bump version to 1.9.0 and update Alert interface with additional properties

--- src/types/alert.ts
+++ src/types/alert.ts
@@ -1,3 +1,5 @@
+import { MediaItem } from "./media-item";
+
 export interface Alert {
   id: string;
   title: string;
@@ -7,5 +9,12 @@ export interface Alert {
   pages: {
     id: string;
     slug: string;
+    path: string;
   }[];
+  start_at: string;
+  end_at: string;
+  media: MediaItem;
+  content: string | null;
+  cta_label: string | null;
+  cta_url: string | null;
 }
